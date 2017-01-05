925cd73b3d55950be1f75fdc98b7abde47a7c204
venkat24 <venkatramansrikanth@outlook.com>
2017-01-05 22:42:28 +0530

    Session check middleware for Login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         //\App\Http\Middleware\VerifyCsrfToken::class,
         \App\Http\Middleware\SetResponseHeaders::class,
+        \App\Http\Middleware\CheckSession::class,
     ];
 
     /**
---
+++ app/Http/Middleware/CheckSession.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Session;
+use Log;
+use Sangria\JSONResponse;
+
+class checkSession
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $pragyanId = Session::get('pragyanId');
+        if($pragyanId)
+            return $next($request);
+        else {
+            $status_code = 401; //unauthorized
+            $message = "Session expired. Please login again.";
+            Log::info('Logged out');
+            //for post routes, throw a 401
+            if($request->isMethod('post')) {
+                return JSONResponse::response($status_code, $message);
+            }
+            //for get routes, redirect to login page
+            return redirect('/');
+        }
+    }
+}
+
