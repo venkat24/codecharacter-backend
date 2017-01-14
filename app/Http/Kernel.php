c6d1dc65a71b96e865e5d08e00827a4f274696a9
venkat24 <venkatramansrikanth@outlook.com>
2017-01-15 05:14:49 +0530

    Response Headers Middleware no longer Global

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,6 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         //\App\Http\Middleware\VerifyCsrfToken::class,
-        \App\Http\Middleware\SetResponseHeaders::class,
     ];
 
     /**
@@ -28,6 +27,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'checkSession' => \App\Http\Middleware\CheckSession::class,
+        'setResponseHeaders' => \App\Http\Middleware\SetResponseHeaders::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
--- app/Http/Middleware/SetResponseHeaders.php
+++ app/Http/Middleware/SetResponseHeaders.php
@@ -15,16 +15,8 @@ class SetResponseHeaders
      */
     public function handle($request, Closure $next)
     {
-        /** 
-         * The JSON Content Type response header is being set globally
-         * on all requests. Change this into a route middleware for only
-         * the JSON APIs later. This is purely for testing.            
-         */
-        if($request->isMethod('post'))  {          
-            $response = $next($request);
-            return $response->header('Content-Type', 'application/json');
-        }
-        return $next($request);
+        $response = $next($request);
+        return $response->header('Content-Type', 'application/json');
     }
 }
 
