0474ee0240ed5e534586b041fec6fed52c525cd7
venkat24 <venkatramansrikanth@outlook.com>
2017-01-16 23:22:30 +0530

    Notitifications View and Styles Update

---
+++ resources/views/notifications.blade.php
@@ -0,0 +1,29 @@
+@extends('base')
+
+@section('links')
+  <title>Welcome to Foundation | Banded</title>
+  <link rel="stylesheet" href="{{asset('stylesheets/notifications.css')}}">
+@endsection
+
+@section('main')
+
+  <div class="row">
+    <div class="twelve columns">
+      <h2 class="text-center" id="main-heading">Notifications</h2>
+      <hr />
+    </div>
+  </div>
+  <div class="row">
+      @foreach ($notifications as $notif)
+    <div class="twelve columns callout-container primary">
+      <div class="callout">
+        <h5>{{$notif->title}}</h5>
+        <p>{{$notif->message}}</p>
+        <a href="#">It's dangerous to go alone, take this.</a>
+      </div>
+    </div>
+      @endforeach
+  </div>
+  
+@endsection
+
