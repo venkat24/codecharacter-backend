a512a21ef46f94be483bc7ad370145da8679368a
venkat24 <venkatramansrikanth@outlook.com>
2017-01-16 22:32:57 +0530

    Leaderboard Table Migration

---
+++ database/migrations/2017_01_16_165818_create_leaderboard_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaderboardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leaderboard', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('level')->default(1);
+            $table->integer('score')->default(0);
+            $table->integer('teamId'); //Foreign Key to Teams
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('leaderboard');
+    }
+}
