a4663c46dd3d885c75aa0733b60ab4d919203821
venkat24 <venkatramansrikanth@outlook.com>
2017-01-20 01:23:18 +0530

    Login Form formatting changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,22 +7,21 @@
 
 @section('main')
 <div class="row">
-	<div class="large-4 columns">
+	<div class="four columns">
   </div>
-  <div class="large-4 columns">
+  <div class="four columns">
     <div class="row column log-in-form">
       <h4 class="text-center">Log in with your Pragyan account</h4>
       <label>Email
-        <input type="text" placeholder="somebody@example.com">
+        <input type="text" id="username" placeholder="somebody@example.com">
       </label>
       <label>Password
-        <input type="text" placeholder="Password">
+        <input type="password" id="password" placeholder="Password">
       </label>
-      <input id="show-password" type="checkbox"><label for="show-password">Show password</label>
-      <p><a type="submit" class="button expanded">Log In</a></p>
+      <p><button class="button expanded" onclick="login();return false;">Log In</button></p>
     </div>
 	</div>
-	<div class="large-4 columns">
+	<div class="four columns">
   </div>
 </div>
 	<script src="{{asset('javascripts/login.js')}}"></script>
