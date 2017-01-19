5bfef1d6937482a6635cee3f332a31d9c55c69f1
venkat24 <venkatramansrikanth@outlook.com>
2017-01-20 01:22:49 +0530

    Table Layout for leaderboard display

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -15,7 +15,27 @@
     </div>
   </div>
   <div class="row">
-      @foreach ($leaderboard as $user)
+    <div class="twelve columns callout-container primary">
+      <table>
+        <tbody>
+    @foreach ($leaderboard as $user)
+          <tr>
+            <td>
+              {{$user->teamName}}
+            </td>
+            <td>
+              {{$user->level}}
+            </td>
+            <td>
+              {{$user->score}}
+            </td>
+          </tr>
+    @endforeach
+        </tbody>
+      </table>
+    </div>
+    <!-- 
+    @foreach ($leaderboard as $user)
     <div class="twelve columns callout-container primary">
       <div class="callout">
         <h5>{{$user->teamName}}</h5>
@@ -24,7 +44,8 @@
         <a href="#">It's dangerous to go alone, take this.</a>
       </div>
     </div>
-      @endforeach
+    @endforeach
+    -->
   </div>
 @endsection
 
