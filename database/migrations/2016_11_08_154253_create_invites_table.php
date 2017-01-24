6364d4a6154a00587dbc1bfb484b3fbbcfd3028e
venkat24 <venkatramansrikanth@outlook.com>
2017-01-24 12:17:43 +0530

    Change default column value in Invites Status

--- database/migrations/2016_11_08_154253_create_invites_table.php
+++ database/migrations/2016_11_08_154253_create_invites_table.php
@@ -16,7 +16,7 @@ class CreateInvitesTable extends Migration
             $table->increments('id');
             $table->integer('fromTeamId')->unsigned();
             $table->integer('toRegistrationId')->unsigned();
-            $table->enum('status', ['SENT','ACCEPTED'])->default('ACCEPTED');
+            $table->enum('status', ['SENT','ACCEPTED'])->default('SENT');
             $table->timestamps();
         });
     }
