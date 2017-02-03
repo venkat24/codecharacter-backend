a5237b8650ed0aa6ccea5b4ea8fa41085779f156
Anirudh Sundar <anirudhsundar@hotmail.com>
2017-02-03 21:41:30 +0530

    Add remove and leave features to team page

--- public/javascripts/teams.js
+++ public/javascripts/teams.js
@@ -1,38 +1,8 @@
-// function check() {
-// 	console.log('checking');
-// 	var span = document.getElementById('check');
-// 	var request = $.ajax({
-// 		url: SITE_BASE_URL + '/api/check_if_team_exists',
-// 		type: 'GET',
-// 		data: {
-// 		  teamName: USER_DATA.teamName
-// 		},
-// 	});
-// 	console.log('lol');
-
-// 	request.done(function(data) {
-// 		console.log(data);
-// 		if (data.status_code == 200) {
-// 			if (data.message = "EXISTS")
-// 				span.innerHTML = "X";
-// 			else span.innerHTML = "✓";
-// 		}
-// 	});
-// 	request.fail(function(jqXHR, textStatus, err) {
-// 		return console.log(err.toString());
-// 	});	
-// }
-
-$(document).ready(function () {
-    teamData.lockTeamName();
-});
-
 Vue.component('team-member', {
 	props: ['member'],
 	template: '<div> <br /><input class="memberName" :value="member.name" placeholder="name" disabled /> <input class="memberEmail" :value="member.email" placeholder="email" /> <span class="buttonContainer"><button class="inviteButton" onclick="invite(this)">Send Invite</button></span> </div>'
 });
 
-console.log("TEAMNAME : " + USER_DATA.teamName);
 var teamData = new Vue({
 	el: "#team-info",
 	data: {
@@ -40,28 +10,17 @@ var teamData = new Vue({
 		teamMemberSeen: false,
 		newTeamName: USER_DATA.teamName,
 		buttonSeen: false,
-		teamMembers: [
-			// {name: 'testName', email: 'email@pragyan.com'}
-		]
+		teamMembers: []
 	},
 	methods: {
-        lockTeamName: function() {
-            console.log("Executing Lock..");
-            if(this.newTeamName !== "") {
-                document.getElementById('team-name').disabled = true;
-                console.log("Actual Locking");
-            }
-        },
 		create: function() {
-			var teamName = this.newTeamName;
+			var teamName = document.getElementById('team-name').value;
 			var sentData = { teamName: teamName, leaderEmail: USER_DATA.userEmail };
 			var request = $.ajax({
 				url: SITE_BASE_URL + '/api/create_team',
 				type: 'POST',
 				data: sentData
 			});
-			console.log("create " + teamName);
-			console.log(sentData);
 
 			// else
 			// 	var request = $.ajax({
@@ -73,10 +32,8 @@ var teamData = new Vue({
 			// 	});
 
 			request.done(function(data) {
-				console.log("hi");
 				if (data.status_code == 200) {
-					console.log("hidone");
-					teamData.buttonText = 'Rename';
+					teamData.buttonText = 'Delete';
 					teamData.teamMemberSeen = true;
 					teamData.buttonSeen = true;
 					teamData.teamMembers.push({name: USER_DATA.userName, email: USER_DATA.userEmail});
@@ -84,10 +41,8 @@ var teamData = new Vue({
 					setTimeout(function() {
 						document.getElementsByClassName('inviteButton')[0].style.visibility = 'hidden';
 						document.getElementsByClassName('memberEmail')[0].disabled = true;
-					}, 10);
-                } else if (data.status_code == 400) {
-                    alert(data.message);
-                }
+					}, 30);
+				}
 			});
 			request.fail(function(jqXHR, textStatus, err) {
 				return console.log(err.toString());
@@ -118,6 +73,7 @@ function invite(button) {
 	});
 
 	request.done(function(data) {
+		console.log(data);
 		if (data.status_code == 200) {
 			if (teamData.teamMembers.length < 3)
 				teamData.buttonSeen = true;
@@ -125,11 +81,9 @@ function invite(button) {
 			email[email.length - 1].disabled = true;
 			names = document.getElementsByClassName('memberName');
 			names[names.length-1].value = data.message;
-			buttons[buttons.length-1].innerHTML = " Invite Sent";
+			buttons[buttons.length-1].innerHTML = "<button class='deleteButton' onclick='delete(this)'>Cancel Invite</button>"
 			// button.disabled = true;
-		} else if (data.status_code == 400) {
-                    alert(data.message);
-        }
+		}
 	});
 	request.fail(function(jqXHR, textStatus, err) {
 		return console.log(err.toString());
@@ -168,12 +122,14 @@ function invite(button) {
 			teamData.buttonSeen = true;
 			setTimeout(function() {
 				for (var i = 0; i < data.message.length; i++) {
-					if (data.message[i].status == 'ACCEPTED')
-						document.getElementsByClassName('buttonContainer')[i].innerHTML = '';
-					else document.getElementsByClassName('buttonContainer')[i].innerHTML = ' Invite Sent';
 					document.getElementsByClassName('memberEmail')[i].disabled = true;
+					if (data.message[i].status == 'LEADER')
+						document.getElementsByClassName('buttonContainer')[i].innerHTML = "<button class='leaveButton' onclick=''>Leave</button>";
+					else if (data.message[i].status == 'ACCEPTED')
+						document.getElementsByClassName('buttonContainer')[i].innerHTML = "<button class='removeButton' onclick=''>Remove Member</button>";
+					else document.getElementsByClassName('buttonContainer')[i].innerHTML = "<button class='deleteButton' onclick=''>Cancel Invite</button>";
 				}
-			}, 10);
+			}, 30);
 		}
 	});
 	request.fail(function(jqXHR, textStatus, err) {
