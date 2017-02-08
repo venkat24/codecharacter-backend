f198b70dd7b63d1573ae8cdd0926bfc80f31b35f
venkat24 <venkatramansrikanth@outlook.com>
2017-02-08 18:14:37 +0530

    Watcher Script and Docker Start Script

--- simulator/test.py
+++ simulator/test.py
@@ -1,2 +1,28 @@
+import sqlalchemy
+import os, sys, time, shutil
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:pwd@172.18.0.2:3306/code_character')
+
+startFlag = True
+currentSubmissions = 0
+
+def runSubmissions(submissions, currentSubmissions, submissionCount):
+    for i in range(currentSubmissions,submissionCount):
+        print(submissions[i])
+        shutil.copy(submissions[i]['sourceCodePath'],os.getcwd())
+
 while True:
-    print("Hello World")
+    submissions = engine.execute('SELECT * FROM submissions');
+    submissionCount = 0
+    submissionsList= []
+    for row in submissions:
+        submissionsList.append(dict(zip(row.keys(), row)))
+        submissionCount+=1
+    if startFlag:
+        currentSubmissions = submissionCount
+        startFlag = False
+    if currentSubmissions != submissionCount:
+        runSubmissions(submissionsList, currentSubmissions, submissionCount)
+    print(submissionCount)
+    currentSubmissions = submissionCount
+    time.sleep(5)
