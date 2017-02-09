4a1a8532f11a223aa380591a4cbb1b051248c05b
venkat24 <venkatramansrikanth@outlook.com>
2017-02-09 12:59:48 +0530

    Dummy submit view before submissions open

---
+++ resources/views/submit-dummy.blade.php
@@ -0,0 +1,20 @@
+@extends('base')
+
+@section('links')
+  <title>Submit - Code Character</title>
+  <link rel="stylesheet" href="{{asset('stylesheets/submit.css')}}">
+@endsection
+
+@section('main')
+  <div class="row">
+    <div class="twelve columns">
+      <h2 class="text-center" id="main-heading">Submit Code</h2>
+      <hr />
+    </div>
+  </div>
+  <div class="row">
+    <div class="twelve columns">
+      <h5 style="text-align:center">Submissions have not yet been opened</h5>
+    </div>
+  </div>
+@endsection
