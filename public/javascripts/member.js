46b604bdc79db246b4266d71f14dc6ce26d3af54
venkat24 <venkatramansrikanth@outlook.com>
2017-02-09 13:18:04 +0530

    Confirmations on leave team

--- public/javascripts/member.js
+++ public/javascripts/member.js
@@ -1,21 +1,23 @@
 function leave(button) {
-	var request = $.ajax({
-		url: SITE_BASE_URL + '/api/leave_team',
-		type: 'POST',
-		data: {
-			teamName: USER_DATA.teamName,
-			userEmail: USER_DATA.userEmail,
-		},
-	});
+    if(confirm("Are you SURE you want to leave this team?")) {
+        var request = $.ajax({
+            url: SITE_BASE_URL + '/api/leave_team',
+            type: 'POST',
+            data: {
+                teamName: USER_DATA.teamName,
+                userEmail: USER_DATA.userEmail,
+            },
+        });
 
-	request.done(function(data) {
-		if (data.status_code == 200) {
-			alert(data.message);
-			location.reload();
-		} else alert(data.message);
-	});
-	request.fail(function(jqXHR, textStatus, err) {
-		return console.log(err.toString());
-	});
+        request.done(function(data) {
+            if (data.status_code == 200) {
+                alert(data.message);
+                location.reload();
+            } else alert(data.message);
+        });
+        request.fail(function(jqXHR, textStatus, err) {
+            return console.log(err.toString());
+        });
+    }
 }
 
