76a05f804ec7f7269b17009f34dce878534da54f
venkat24 <venkatramansrikanth@outlook.com>
2017-02-21 02:16:39 +0530

    Migration to add queue table

---
+++ database/migrations/2017_02_20_132242_create_queue_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQueueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('queue', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('submissions_id');
+            $table->integer('attempts')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('queue');
+    }
+}
